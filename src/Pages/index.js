3c8b44cae7b5488b9507409d3d8effc19befafbe
dohun <dhp137qkr@gmail.com>
2025-06-09 17:13:50 +0900

    commit

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,14 +1,14 @@
-export { default as Login } from './Login';
-export { default as Register } from './Register';
+export { default as Login } from './Auth/Login';
+export { default as Register } from './Auth/Register';
 export { default as Mypage } from './Mypage';
 export { default as Notfound } from './Notfound';
 
-export { default as CommunityNew } from './CommunityNew';
-export { default as CommunityBoard } from './CommunityBoard';
-export { default as CommunityUpdate } from './CommunityUpdate';
-export { default as CommunityDetail } from './CommunityDetail';
+export { default as CommunityNew } from './Community/CommunityNew';
+export { default as CommunityBoard } from './Community/CommunityBoard';
+export { default as CommunityUpdate } from './Community/CommunityUpdate';
+export { default as CommunityDetail } from './Community/CommunityDetail';
 
-export { default as RecruitBoard } from './RecruitBoard';
-export { default as RecruitDetail } from './RecruitDetail';
-export { default as RecruitNew } from './RecruitNew';
-export { default as RecruitUpdate } from './RecruitUpdate';
+export { default as RecruitBoard } from './Recruit/RecruitBoard';
+export { default as RecruitDetail } from './Recruit/RecruitDetail';
+export { default as RecruitNew } from './Recruit/RecruitNew';
+export { default as RecruitUpdate } from './Recruit/RecruitUpdate';
